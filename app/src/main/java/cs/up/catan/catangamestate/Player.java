f971fe69ba81ef87c7e69cb6712b82c45c3a68e1
alexweininger <alex.weininger@live.com>
2018-10-09 18:02:05 -0700

    fixed warnings fixed toString

--- app/src/main/java/cs/up/catan/catangamestate/Player.java
+++ app/src/main/java/cs/up/catan/catangamestate/Player.java
@@ -5,21 +5,24 @@ import java.util.HashMap;
 
 public class Player extends GameState {
 
-    //instance variables
+	//instance variables
 
-	int localScore; // score of player that accounts for the players dev cards, must greater or eqaul to global score
-	HashMap<Integer, Integer> resources = new HashMap<Integer, Integer>();
-	ArrayList<DevelopmentCards> developmentCards = new ArrayList<DevelopmentCards>();
+	private int localScore; // score of player that accounts for the players dev cards, must greater or eqaul to global score
+	private HashMap<Integer, Integer> resources;
+	private ArrayList<DevelopmentCards> developmentCards;
 
-    public Player(int settlementCount, int cityCount, int roadCount, boolean isTurn, int woodCount, int oreCount, int brickCount, int wheatCount, int woolCount) {
+	public Player(int localScore, HashMap<Integer, Integer> resources, ArrayList<DevelopmentCards> developmentCards) {
+		this.localScore = localScore;
+		this.resources = resources;
+		this.developmentCards = developmentCards;
+	}
 
-    }
-
-    @Override
-    public String toString() {
-        return "The player has "+ settlementCount + " Settlements, " + cityCount + " Cities, " +
-                roadCount + " Roads." + "Is it their turn: " + isTurn + ". In their inventory" +
-                "they have:" + brickCount + " Brick, " + oreCount + " Ore, " + woolCount +
-                " Sheep, " + wheatCount + " Wheat, and " + woodCount + " wood.";
-    }
+	@Override
+	public String toString() {
+		return "Player{" +
+				"localScore=" + localScore +
+				", resources=" + resources +
+				", developmentCards=" + developmentCards +
+				'}';
+	}
 }
