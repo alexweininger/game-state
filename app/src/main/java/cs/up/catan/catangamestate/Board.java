335238929fe54dbfb211427d8b5d388618005f37
alexweininger <alex.weininger@live.com>
2018-10-10 23:39:49 -0700

    init board

--- app/src/main/java/cs/up/catan/catangamestate/Board.java
+++ app/src/main/java/cs/up/catan/catangamestate/Board.java
@@ -4,30 +4,31 @@ import java.util.ArrayList;
 
 public class Board extends GameState {
 
-    private ArrayList<ArrayList<Integer>> intersectionRings;
-    private ArrayList<ArrayList<Integer>> hexagonRings;
+    private ArrayList<ArrayList<Integer>> intersectionRings = new ArrayList<ArrayList<Integer>>();
+    private ArrayList<ArrayList<Integer>> hexagonRings = new ArrayList<ArrayList<Integer>>();
 
-    public Board(){
-
+    public Board() {
+        ArrayList<Integer> temp = new ArrayList<>();
+        temp.add(6);
+        this.intersectionRings.add(temp);
+        this.intersectionRings.add(temp);
     }
 
     @Override
     public String toString() {
-        String intsectionRingsString = "";
+        String intersectionRingsString = "";
         String hexagonRingString = "";
 
-        for (int i = 0; i < intersectionRings.size(); i++)
-        {
-            intsectionRingsString = intersectionRings.get(i) + " ";
+        for (int i = 0; i < intersectionRings.size(); i++) {
+            intersectionRingsString = intersectionRings.get(i) + " ";
         }
 
-        for (int i = 0; i < hexagonRings.size(); i++)
-        {
+        for (int i = 0; i < hexagonRings.size(); i++) {
             hexagonRingString = hexagonRings.get(i) + " ";
         }
 
 
-        return "Intersection Rings: " + intsectionRingsString + "\n" +
+        return "Intersection Rings: " + intersectionRingsString + "\n" +
                 "Hexagon Rings: " + hexagonRingString + "\n";
     }
 }
