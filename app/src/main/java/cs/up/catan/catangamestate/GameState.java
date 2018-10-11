942a0e48216a282fad5435c483f218e57757c604
alexweininger <alex.weininger@live.com>
2018-10-10 22:01:08 -0700

    implementing game state class

--- app/src/main/java/cs/up/catan/catangamestate/GameState.java
+++ app/src/main/java/cs/up/catan/catangamestate/GameState.java
@@ -1,8 +1,14 @@
 package cs.up.catan.catangamestate;
 
+import java.util.ArrayList;
+
 public class GameState {
 
     Dice dice = new Dice();
+    int currentPlayer;
+    int currentDiceSum;
+    ArrayList<Player> playerList;
+
 
     public GameState() {
 
