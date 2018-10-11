be59965ede1105eecfa1e757c082f46c4d374cbb
alexweininger <alex.weininger@live.com>
2018-10-11 15:51:13 -0700

    initialize and define intersectionIdRings and hexagonIdRings in board class

--- app/src/main/java/cs/up/catan/catangamestate/Board.java
+++ app/src/main/java/cs/up/catan/catangamestate/Board.java
@@ -1,18 +1,20 @@
 package cs.up.catan.catangamestate;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 
 public class Board extends GameState {
 
-    private ArrayList<ArrayList<Integer>> intersectionRings = new ArrayList<ArrayList<Integer>>();
-    private ArrayList<ArrayList<Integer>> hexagonRings = new ArrayList<ArrayList<Integer>>();
+    private ArrayList<ArrayList<Integer>> intersectionIdRings = new ArrayList<ArrayList<Integer>>();
+    private ArrayList<ArrayList<Integer>> hexagonIdRings = new ArrayList<ArrayList<Integer>>();
 
     public Board() {
-        ArrayList<Integer> temp = new ArrayList<>();
-        temp.add(6);
-        this.intersectionRings.add(temp);
-        this.intersectionRings.add(temp);
+        int hexagonRingSize, intersectionRingSize;
+        for (int i = 0; i < 3; i++) {
+            hexagonRingSize = i * 6;
+            intersectionRingSize = ((i * 2) + 1) * 6;
+            this.hexagonIdRings.add(new ArrayList<Integer>(hexagonRingSize));
+            this.hexagonIdRings.add(new ArrayList<Integer>(intersectionRingSize));
+        }
     }
 
     @Override
@@ -20,12 +22,12 @@ public class Board extends GameState {
         String intersectionRingsString = "";
         String hexagonRingString = "";
 
-        for (int i = 0; i < intersectionRings.size(); i++) {
-            intersectionRingsString = intersectionRings.get(i) + " ";
+        for (int i = 0; i < intersectionIdRings.size(); i++) {
+            intersectionRingsString = intersectionIdRings.get(i) + " ";
         }
 
-        for (int i = 0; i < hexagonRings.size(); i++) {
-            hexagonRingString = hexagonRings.get(i) + " ";
+        for (int i = 0; i < hexagonIdRings.size(); i++) {
+            hexagonRingString = hexagonIdRings.get(i) + " ";
         }
 
 
