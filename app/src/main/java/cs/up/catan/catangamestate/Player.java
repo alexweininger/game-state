b568ef159c3ed70a45c492f7ce16ab22b282358d
malin383 <nirajlmali@gmail.com>
2018-10-10 22:12:35 -0700

    Moved action methods; preparing for massive merge

--- app/src/main/java/cs/up/catan/catangamestate/Player.java
+++ app/src/main/java/cs/up/catan/catangamestate/Player.java
@@ -28,46 +28,6 @@ public class Player extends GameState {
 				'}';
 	}
 
-	/*rollDice()
-	    --Player will roll dice and send integer to game state
-	TODO
-	 */
 
-	/*tradePort()
-	    --Player will ask to trade with a port and send info to game state
-	TODO
-	 */
-
-	/*build()
-	    --Player selects item to build and sends to gameState which will process selection
-	TODO
-	 */
-
-	/*useDevCard()
-	    --Player will select a development card they own and use it; gamestate will
-	    --determine legality and then carry out development cards function
-	TODO
-	 */
-
-	/*robberDiscard()
-	    --Player chooses cards to discard if they own more than 7 cards and robber is activated
-	TODO
-	 */
-
-	/*confirmAction();
-	    --Gamestate will ask player to confirm action; based on player's decision, gamestate decides
-	    --On next steps
-	TODO
-	 */
-
-	/*rollDice();
-	    --Player will send action to gamestate to roll dice
-	TODO
-	 */
-
-	/*moveRobber();
-	    --Called dice value is a 7
-	TODO
-	 */
 
 }
