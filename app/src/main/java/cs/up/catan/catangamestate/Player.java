034c64ade8749307f5d705afcf55a3ad833f70ec
alexweininger <alex.weininger@live.com>
2018-10-11 21:23:16 -0700

    dice game state player

--- app/src/main/java/cs/up/catan/catangamestate/Player.java
+++ app/src/main/java/cs/up/catan/catangamestate/Player.java
@@ -7,27 +7,30 @@ public class Player {
 
 	//instance variables
 	private int localScore; // score of player that accounts for the players dev cards, must greater or equal to global score
-	private HashMap<Integer, Integer> resources = new HashMap<Integer, Integer>(); // k: resource id, v: resource count
+	private int publicScore;
+	private HashMap<String, Integer> resources = new HashMap<String, Integer>(); // k: resource id, v: resource count
 	private ArrayList<Integer> developmentCards = new ArrayList<Integer>(); // ArrayList of the development card IDs the player owns
 
 	// constructor
 	public Player() {
 		this.localScore = 2;
-		this.resources.put(22, 33);
-		this.developmentCards.add(10);
+		this.publicScore = 4;
+		this.resources.put("Logs", 3);
+        this.resources.put("Bricks", 1);
+        this.resources.put("Ore", 1);
+        this.resources.put("Wool", 2);
+		this.developmentCards.add(1);
 	}
 
 	public Player(Player player) {
-        this.localScore = player.localScore;
+		this.publicScore = player.publicScore;
+		this.localScore = player.localScore;
         this.developmentCards = player.developmentCards;
         this.resources = player.resources;
     }
 
 	@Override
 	public String toString(){
-		return "Player: \nLocal Score = " + localScore + "\nResources = " + resources + "\nDevelopment Cards = " + developmentCards;
+		return "Player: \nLocal Score = " + this.localScore + "\nPublic score = " + this.publicScore + "\nResources = " + this.resources + "\nDevelopment Cards = " + this.developmentCards + "\n";
 	}
-
-
-
 }
