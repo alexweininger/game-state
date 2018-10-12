baf6bc66a873f6c25bb80b697995e2bbf04f2ba6
Andrew <andrewespn@hotmail.com>
2018-10-11 20:51:37 -0700

    Fixed Player class

--- app/src/main/java/cs/up/catan/catangamestate/Player.java
+++ app/src/main/java/cs/up/catan/catangamestate/Player.java
@@ -3,26 +3,23 @@ package cs.up.catan.catangamestate;
 import java.util.ArrayList;
 import java.util.HashMap;
 
-public class Player extends GameState {
+public class Player {
 
 	//instance variables
 	private int localScore; // score of player that accounts for the players dev cards, must greater or equal to global score
 	private HashMap<Integer, Integer> resources = new HashMap<Integer, Integer>(); // k: resource id, v: resource count
 	private ArrayList<Integer> developmentCards = new ArrayList<Integer>(); // ArrayList of the development card IDs the player owns
-	private ArrayList<Building> buildings = new ArrayList<Building>();
 
 	// constructor
 	public Player() {
-	//	this.localScore = 0;
-	//	this.resources.put(22, 33);
-	//	this.developmentCards.add(10);
-	//	this.buildings.add(new Building());
+		this.localScore = 2;
+		this.resources.put(22, 33);
+		this.developmentCards.add(10);
 	}
 
 	@Override
-	public String toString() {
-		return "Player[" + "localScore:" + localScore + ", resources:" + resources +
-				", developmentCards:" + developmentCards + "]\n";
+	public String toString(){
+		return "Player: \nLocal Score = " + localScore + "\nResources = " + resources + "\nDevelopment Cards = " + developmentCards;
 	}
 
 
