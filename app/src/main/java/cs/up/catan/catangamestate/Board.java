4cd4ec2d1b654d8f1c31021df8d65bd81c52e24d
alexweininger <alex.weininger@live.com>
2018-10-23 14:53:33 -0700

    board work

--- app/src/main/java/cs/up/catan/catangamestate/Board.java
+++ app/src/main/java/cs/up/catan/catangamestate/Board.java
@@ -38,6 +38,7 @@ public class Board {
     /*  hexagonAdjacencyGraph is a 2d ArrayList that holds adjacency information for each hexagon.    *
      *  To check if hexagon 2 and 7 are adjacent, we can get the value located at list.get(2).get(7); */
     private ArrayList<ArrayList<Boolean>> hexagonAdjacencyGraph = new ArrayList<ArrayList<Boolean>>(18);
+    private boolean[][] hGraph = new boolean[19][19];
 
     private ArrayList<ArrayList<Boolean>> intersectionAdjecencyGraph = new ArrayList<ArrayList<Boolean>>(36); // TODO
 
@@ -48,10 +49,18 @@ public class Board {
     public Board() {
 
         populateHexagonIds();
-        pupulateIntersectionIds();
+        populateIntersectionIds();
+        for (int i = 0; i < 2; i++) { // rings
+            for (int j = 0; j < this.hexagonIdRings.get(i).size(); j++) { // ids
+                this.hGraph[i][j] = false;
+            }
+        }
 
-        for (int i = 0; i < this.hexagonIdRings.size(); i++) {
-            for (int j = 0; j < this.hexagonIdRings.get(i).size(); j++) {
+
+        for (int i = 0; i < 2; i++) { // rings
+            for (int j = 0; j < this.hexagonIdRings.get(i).size(); j++) { // ids
+
+                // i and j are only 0 once and never are 0 again 0, 0 = center
                 /* TODO implement
                  * for each hexagon in hexagonIdRings:
                  *   1. check the next hexagon in the same ring
@@ -60,13 +69,52 @@ public class Board {
                  *     a. corner vs. non-corner hexagons = if j % i == 0
                  *     b. sextants (0-5), calculated with sextant = j / i;
                  */
+                int sextant = 0;
+                boolean corner = false;
+                if (i == 0) {
+                    sextant = j;
+                    corner = true;
+                } else {
+
+                    sextant = j / i;
+                    corner = j % i == 0;
+                }
+
+
+
+                this.hGraph[getId(i, j)][getId(i + 1, j + sextant)] = true;
+                this.hGraph[getId(i, j)][getId(i + 1, j + sextant + 1)] = true;
+
+//                if (corner) { // check if hexagon is a corner
+//                    int size = 1;
+//                    if (i == 1) {
+//                        size = 6;
+//                    } else {
+//                        size = 12;
+//                    }
+//                    Log.d("dev", "sextant= " + sextant + " i= " + i + " j= " + j);
+//                    Log.d("dev", "id value wrapped: " + Math.abs(j - 1 + sextant) % size);
+//                    hGraph[i + 1][Math.abs(j - 1 + sextant)] = true;
+//                }
             }
         }
+        StringBuilder str = new StringBuilder();
+        str.append("\n");
+        for (int i = 0; i < hGraph.length; i++) {
+            for (int j = 0; j < hGraph[i].length; j++) {
+                str.append(i).append(" ").append(j).append(" ").append(hGraph[i][j]).append(" ");
+            }
+            str.append("\n");
+        }
+        Log.d("dev", "" + str.toString());
     } // end constructor
 
+    public int getId(int ring, int col) {
+        return hexagonIdRings.get(ring).get(col);
+    }
 
     // populating hexagonIdRings with hex IDs (0-18, 19 hexagons)
-    public void populateHexagonIds() {
+    private void populateHexagonIds() {
         int id = 0;
         for (int i = 0; i < 3; i++) {
             this.hexagonIdRings.add(new ArrayList<Integer>());
@@ -82,16 +130,25 @@ public class Board {
         }
     }
 
-    public void pupulateIntersectionIds() {
+    private void populateIntersectionIds() {
         int id = 0;
         for (int i = 0; i < 3; i++) {
             this.intersectionIdRings.add(new ArrayList<Integer>());
             for (int j = 0; j < ((2 * i) + 1) * 6; j++) {
                 this.intersectionIdRings.get(i).add(id);
-                Log.d("dev", "adding " + id + " to intersection id list");
-                id++;
             }
         }
+    }
+
+    public boolean checkHexagonAdjacency(int id1, int id2) {
+
+        /*
+         * 1. if in same ring == true
+         * 2. check graph
+         */
+
+
+        return false;
     }
 
     /**
