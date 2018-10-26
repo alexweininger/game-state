9882d023564f305447002a49bde19cb13be60684
alexweininger <alex.weininger@live.com>
2018-10-25 17:56:01 -0700

    condensed fluffy code made by DB :)

--- app/src/main/java/cs/up/catan/catangamestate/DevelopmentCards.java
+++ app/src/main/java/cs/up/catan/catangamestate/DevelopmentCards.java
@@ -7,17 +7,16 @@ package cs.up.catan.catangamestate;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Random;
 
 public class DevelopmentCards {
 
     private ArrayList<Integer> developmentCards = new ArrayList<Integer>(); // ArrayList of the development card in the deck
-    private HashMap<String, Integer> checkResources = new HashMap<>();
+    private HashMap<String, Integer> resourceCost = new HashMap<>();
 
     //default instance variable
     private String name;
     private boolean isPlayable;
-
-    // hashmap connecting ids to a specifc card
 
     public DevelopmentCards(String name) {
         this.name = name;
@@ -28,9 +27,9 @@ public class DevelopmentCards {
      * creates a deck of int representing the exact number each type of card
      */
     public void generateDevCardDeck() {
-        checkResources.put("Ore", 1);
-        checkResources.put("Wheat", 1);
-        checkResources.put("Sheep", 1);
+        resourceCost.put("Ore", 1);
+        resourceCost.put("Wheat", 1);
+        resourceCost.put("Sheep", 1);
 
         int[] devCardCounts = {14, 5, 2, 2, 2};
         for (int i = 0; i < devCardCounts.length; i++) {
@@ -61,27 +60,24 @@ public class DevelopmentCards {
      * @return the random dev card the player drew
      */
     public DevelopmentCards getRandomCard() {
-        int random = (int) Math.random() * 25;
-        int cardChosen = developmentCards.get(random);
-        developmentCards.remove(random);
-        switch (cardChosen) {
+        Random random = new Random();
+        int randomDevCard = random.nextInt(developmentCards.size() - 1);
+        int drawnDevCard = developmentCards.get(randomDevCard);
+        developmentCards.remove(randomDevCard);
+        switch (drawnDevCard) { // switch to create new dev card
             case 0:
-                Knight knight = new Knight();
-                return knight;
+                return new Knight();
             case 1:
-                VictoryPoints vicortyPoints = new VictoryPoints();
-                return vicortyPoints;
+                return new VictoryPoints();
             case 2:
-                RoadDevCard roadBuilding = new RoadDevCard();
-                return roadBuilding;
+                return new RoadDevCard();
             case 3:
-                Monopoly monopoly = new Monopoly();
-                return monopoly;
+                return new Monopoly();
             case 4:
-                YearOfPlenty yearOfPlenty = new YearOfPlenty();
-                return yearOfPlenty;
+                return new YearOfPlenty();
+            default:
+                return null;
         }
-        return null;
     }
 
     /**
